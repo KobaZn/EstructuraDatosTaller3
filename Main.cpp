a427dc9f26e4b0361f1a1b74446d0230b274229e
Benjamín D <benjamin.donoso@alumnos.ucn.cl>
2022-12-07 14:14:31 -0300

    added AVL functions
    funciona:
    - obtenerAltura
    - obtenerFactorBalance
    
    en proceso:
    - rotaciones (LL;RR;LR;RL)
    - insertarAVL

--- Main.cpp
+++ Main.cpp
@@ -12,7 +12,29 @@ struct Nodo{
 };
 
 int calcularPeso(string mes);
+
+struct Nodo* nuevoNodo(string mes){
+    struct Nodo* nodo = new struct Nodo;
+    nodo->mes = mes;
+    nodo->peso = calcularPeso(mes);
+    nodo->l = NULL;
+    nodo->r = NULL;
+    return nodo;
+}
+
+
+int obtenerAltura(Nodo* root);
+int obtenerFactorBalance(Nodo* root);
+
 void recorrerPreOrder(Nodo* root); // RID
+
+void LL(Nodo* root);
+Nodo* RR(Nodo* root); 
+void LR(Nodo* root);
+void RL(Nodo* root);
+
+void insertarAVL(Nodo* root, string mes);
+
 void recorrerInOrder(Nodo* root); // IRD
 void recorrerPostOrder(Nodo* root); // IDR
 
@@ -26,29 +48,33 @@ int main(){
     }
 
     //Arbol de prueba para las funciones de recorrer
+    /* tree
+            En
+            /  \
+        Feb     Mar
+        /   \
+       Abr  May
+    */
+    struct Nodo* Root = nuevoNodo("Enero");
+    Root->r = nuevoNodo("Febrero"); Root->r->r = nuevoNodo("Marzo");
+    //Root->l->l = nuevoNodo("Abril"); Root->l->r = nuevoNodo("Mayo");
 
-    Nodo a,b,c,d,e,f,g,h,i,j;
-    a.peso = 100;
-    b.peso = 50; c.peso = 51;
-    d.peso = 25; e.peso = 26; f.peso = 27; g.peso = 28;
-    h.peso = 15; i.peso = 10; j.peso = 15;
+    Root = RR(Root);
 
-    a.l = &b; a.r = &c;
-    b.l = &d; b.r = &e; c.l = &f; c.r = &g;
-    d.l = NULL; d.r = NULL; e.l = NULL; e.r = NULL; f.l = NULL; f.r = NULL; g.l = NULL; g.r = NULL;
-    //d.l = &h; d.r = &i; e.l = &j; e.r = NULL; f.l = NULL; f.r = NULL; g.l = NULL; g.r = NULL;
-    //h.l = NULL; h.r = NULL; i.l = NULL; i.r = NULL; j.l = NULL; j.r = NULL;
+    cout<<"Pre Order (RID): ";recorrerPreOrder(Root);cout<<endl;
+    cout<<"In Order (IRD): ";recorrerInOrder(Root);cout<<endl;
+    cout<<"Post Order (IDR): ";recorrerPostOrder(Root);cout<<endl;
 
-    Nodo *root = &a;
-
-    recorrerPreOrder(root);
-    cout<<endl;
-    recorrerInOrder(root);
-    cout<<endl;
-    recorrerPostOrder(root);
-
+    Nodo* root = nuevoNodo("");
+    cout<<root->peso<<endl;
+    insertarAVL(root, "Enero");
+    cout<<root->peso<<endl;
+    insertarAVL(root, "Febrero");
+    //insertarAVL(root, "Marzo");
+    cout<<"Pre Order (RID): ";recorrerPreOrder(root);cout<<endl;
     return 0;
 };
+
 int calcularPeso(string mes){
     int valor=0;
     char abecedario[27]= {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','\0'};
@@ -66,8 +92,91 @@ int calcularPeso(string mes){
     return valor;
 }
 
+int obtenerAltura(Nodo* root){
+    if(root == NULL){
+        return 0;
+    }
+    
+    int hl = obtenerAltura(root->l);
+    int hr = obtenerAltura(root->r);
+
+    if(hl>=hr){
+        return hl+1;
+    }
+    return hr+1;
+};
+
+int obtenerFactorBalance(Nodo* root){
+    return obtenerAltura(root->l) - obtenerAltura(root->r);
+};
+
+void LL(Nodo* root){
+    Nodo* aux = root->l->r;
+    root->l->r = root;
+    root->l = aux;
+};
+
+Nodo* RR(Nodo* root){
+    Nodo* newRoot = root->r;
+
+    Nodo* aux = root->r->l;
+    root->r->l = root;
+    root->r = aux;
+
+    return newRoot;
+};
+
+void LR(Nodo* root){
+    RR(root->l);
+    LL(root);
+};
+
+void RL(Nodo* root){
+    LL(root->r);
+    RR(root);
+};
+
+void insertarAVL(Nodo* root, string mes){
+    Nodo* nodo = nuevoNodo(mes);
+    if(root->mes == "" || root->peso == 0){
+        *root = *nodo;
+        return;
+    }
+    if(root->peso > nodo->peso){
+        if(root->l){
+            insertarAVL(root->l, mes);
+        }else{
+            root->l = nodo;
+        }
+    }
+    if(root->peso < nodo->peso){
+        if(root->r){
+            insertarAVL(root->r, mes);
+        }else{
+            root->r = nodo;
+        }
+    }
+
+    int FB = obtenerAltura(root->l) - obtenerAltura(root->r);
+
+    if(FB > 1 && obtenerFactorBalance(root->l) == 1){
+        LL(root);
+    }
+    if(FB > 1 && obtenerFactorBalance(root->l) == -1){
+        LR(root);
+    }
+    if(FB < -1 && obtenerFactorBalance(root->r) == -1){
+        //root = RR(root);
+    }
+    if(FB < -1 && obtenerFactorBalance(root->r) == 1){
+        RL(root);
+    }
+
+};
+
 void recorrerPreOrder(Nodo* root){
-    cout<<root->peso<<" ";
+    cout<<root->mes<<"(";
+    cout<<root->peso<<") ";
     if(root->l){
         recorrerPreOrder(root->l);
     }
@@ -81,7 +190,8 @@ void recorrerInOrder(Nodo* root){
     if(root->l){
         recorrerInOrder(root->l);
     }
-    cout<<root->peso<<" ";
+    cout<<root->mes<<"(";
+    cout<<root->peso<<") ";
     if(root->r){
         recorrerInOrder(root->r);
     }
@@ -95,5 +205,6 @@ void recorrerPostOrder(Nodo* root) {
     if (root->r) {
         recorrerPostOrder(root->r);
     }
-    cout << root->peso << " ";
+    cout<<root->mes<<"(";
+    cout<<root->peso<<") ";
 };
